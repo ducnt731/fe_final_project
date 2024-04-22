1f03c158c40ed39ff077157603fecfde2e83fe26
Manhdc01 <manhnhgch190771@fpt.edu.vn>
2024-04-23 04:55:06 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -16,20 +16,13 @@ import AdminShowTime from './page/admin/adminShowTime';
 import AdminSchedule from './page/admin/adminSchedule';
 import AdminCinema from './page/admin/adminCinema';
 import TheaterCustomer from './page/customer/theaterCustomer';
-<<<<<<< HEAD
 import BookingCustomer from './page/customer/bookingCustomer';
 import BookingSitCustomer from './page/customer/bookingSitCustomer';
 import BookingFoodCustomer from './page/customer/bookingFoodCustomer';
 import PaymentCustomer from './page/customer/paymentCustomer';
-=======
 import GoogleAuthCallback from './page/googleAuthCallBack';
 import ResetPassWord from './components/resetPassWord/resetPass';
 import AdminCinemaHome from './page/adminCinema/adminCinemaHome';
-
-
-
->>>>>>> 1335f20b256519267962af65abaff05378aaab3e
-
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -52,14 +45,11 @@ root.render(
           <Route path="/admin/schedule" element={<AdminSchedule />} />
           <Route path="/admin/cinema" element={<AdminCinema />} />
           <Route path="/theater" element={<TheaterCustomer />} />
-<<<<<<< HEAD
-          <Route path="/booking" element={<BookingCustomer/>}/>
-          <Route path="/booking/bookingsit" element={<BookingSitCustomer/>}/>
-          <Route path="/booking/bookingsit/bookingfood" element={<BookingFoodCustomer/>}/>
-          <Route path="/booking/bookingsit/bookingfood/payment" element={<PaymentCustomer/>}/>
-=======
+          <Route path="/booking" element={<BookingCustomer />} />
+          <Route path="/booking/bookingsit" element={<BookingSitCustomer />} />
+          <Route path="/booking/bookingsit/bookingfood" element={<BookingFoodCustomer />} />
+          <Route path="/booking/bookingsit/bookingfood/payment" element={<PaymentCustomer />} />
           <Route path="/admin-cinema" element={<AdminCinemaHome />} />
->>>>>>> 1335f20b256519267962af65abaff05378aaab3e
         </Routes>
       </BrowserRouter>
     </React.StrictMode>
